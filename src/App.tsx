ab5fe486b839c44ecb56c6e145a431daf064fa72
Fumihiro Teshima <teshima.fu@gmail.com>
2019-07-08 16:02:40 +0900

    add words

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import './App.css';
+import { Convertor } from './Convertor';
+import logo from './gori.png';
+import logo2 from './gori2.png';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>ゴリラ語変換<span style={{ textDecorationLine: 'line-through' }}>クソ</span>ツール</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<{}, { text: string, answer?: string, g?: boolean, t?: boolean }> {
+  constructor(props: any) {
+    super(props);
+    this.state = { text: "" }
+  }
+  private change = () => {
+    let resp = Convertor.convertor(this.state.text);
+    this.setState({ answer: resp.text, g: resp.g, t: resp.t });
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>ゴリラ語変換<span style={{ textDecorationLine: 'line-through' }}>クソ</span>ツール</h1>
+        </header>
+        <div>
+          <h3>ゴリラ語を入れてね？</h3>
+          <input type="text" value={this.state.text} onChange={(e) => this.setState({ text: e.target.value })}></input>
+          <button onClick={this.change}>変換！</button>
+          {this.state.answer &&
+            <div>
+              <div>翻訳結果</div>
+              <div>{this.state.answer}</div>
+              {this.state.g ? <img src={logo2} alt="logo" /> : <img src={logo} alt="logo" />}
+              {this.state.g && <div>※う,ほ,ウ,ホ,っ,ッ以外を使うと怒られちゃうぞ♡</div>}
+
+            </div>
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Convertor.ts
@@ -0,0 +1,51 @@
+
+type Resp = {
+    text: string,
+    g?: boolean,
+    t?: boolean
+}
+
+export class Convertor {
+    public static convertor(input: string): Resp {
+
+        let ans = this.dictionary[input];
+        if (!ans) {
+            if (/[^うほっウホッゴリラごりら]/.test(input))
+                return { text: "おまえ本当にゴリラか？ゴリラ語喋れよ", g: true };
+            ans = this.randomDict[Math.floor(Math.random() * this.randomDict.length)];
+        }
+        return Object.assign({ text: ans }, { t: this.isTT(input) || this.isTT(ans) });
+    }
+
+    private static isTT(input: string): boolean {
+        return input === "とみや" || input === "トミヤ";
+    }
+
+    static readonly dictionary: { [key: string]: string } = {
+        "う": "ん？",
+        "うほ": "おはよう",
+        "ウホ": "やあ",
+        "うっほ": "やっほー",
+        "ウッホ": "元気？",
+        "ゴリラ": "トミヤ",
+        "とみや": "ごりら",
+        "トミヤ": "ゴリラ",
+        "大庭": "人間",
+        "ごりら": "とみや",
+        "うほうほ": "ボルダリング行こうぜ",
+        "ウホウホ": "ボルダリング行こうぜ",
+    }
+
+    static readonly randomDict: string[] = [
+        "ボルダリング行こうぜ",
+        "なんだてめえ",
+        "最近雨が多いですね",
+        "好きなタイプは？",
+        "ちんぽは本当に神",
+        "最近恋ゴリラができたわ",
+        "おまえネットで俺のことばかにしてたよな",
+        "ゴリラを笑うとき、貴様もまたゴリラやぞ",
+        "しょうがないにゃあ",
+        "でぇじょうぶ"
+    ];
+}
